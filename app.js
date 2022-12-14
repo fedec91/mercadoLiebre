80a6e7f7b3009647f8ea158673026ac472f7780b
Fede Caminal <fedecaminal@gmail.com>
2022-12-14 19:22:23 -0300

    primer commit y subida de archivos

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+let port = 3000;
+
+app.use(express.static('public'));
+
+app.listen(port, () => {
+    console.log('Servidor escuchando en puerto ' + port);
+});
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/views/home.html'));
+});
+
+app.get('/register', function(req, res) {
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+});
+
+app.get('/login', function(req, res) {
+    res.sendFile(path.join(__dirname, '/views/login.html'));
+});
